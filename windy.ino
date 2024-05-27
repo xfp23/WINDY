bc1d691373814bb399364c0711a7d3cd64b74fcd
xfp23 <120090946+xfp23@users.noreply.github.com>
2024-05-27 14:22:34 +0800

    Update windy.ino

--- windy.ino
+++ windy.ino
@@ -1,3 +1,13 @@
 #include "pins.h"
 #include <Wire.h>
-#inlcude "freertos/FreeRTOS.h"
+#include 
+unsigned long lastDebounceTime1 = 0;
+unsigned long lastDebounceTime2 = 0;
+unsigned long lastDebounceTime3 = 0;
+unsigned long lastDebounceTime4 = 0;
+unsigned long debounceDelay = 500; //消抖时间
+void setup()
+{
+  Serial.begin(115200);
+  Wire.begin(SDA_PIN,SCL_PIN);
+}
